a08d576b057e900f0885648921a8398a75373dcd
Rennbon <343688972@qq.com>
2021-02-24 11:42:01 +0800

    no message

--- log/log.go
+++ log/log.go
@@ -10,13 +10,5 @@ import (
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		logging.SetLogLevel("*", "INFO")
-		logging.SetLogLevel("dht", "ERROR")
-		logging.SetLogLevel("swarm2", "WARN")
-		logging.SetLogLevel("bitswap", "WARN")
-		//logging.SetLogLevel("pubsub", "WARN")
-		logging.SetLogLevel("connmgr", "WARN")
-		logging.SetLogLevel("advmgr", "DEBUG")
-		logging.SetLogLevel("stores", "DEBUG")
-		logging.SetLogLevel("nat", "INFO")
 	}
 }
