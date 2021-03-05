be1cec97fbbda5a36618aee49b5cfa33253b6cac
Rennbon <343688972@qq.com>
2021-03-05 18:49:17 +0800

    no message

---
+++ core/msg_enum.go
@@ -0,0 +1,54 @@
+package core
+
+type MsgEnum uint64
+
+const (
+	MEUnknown           MsgEnum = 1 << 0
+	MEChainMsg          MsgEnum = 1 << 1
+	MEBlock             MsgEnum = 1 << 2
+	MEDealProposal      MsgEnum = 1 << 3
+	MEDeals             MsgEnum = 1 << 4
+	MEDrawRandomParam   MsgEnum = 1 << 5
+	MESignedVoucher     MsgEnum = 1 << 6
+	MEStorageAsk        MsgEnum = 1 << 7
+	MEAskResponse       MsgEnum = 1 << 8
+	MENetWorkResponse   MsgEnum = 1 << 9
+	MEProviderDealState MsgEnum = 1 << 10
+	MEClientDeal        MsgEnum = 1 << 11
+)
+
+func ContainMsgType(multiME MsgEnum, mt MsgType) bool {
+	me := convertToMsgEnum(mt)
+	return multiME&me == me
+}
+
+func convertToMsgEnum(mt MsgType) MsgEnum {
+	switch mt {
+	case MTUnknown:
+		return MEUnknown
+	case MTChainMsg:
+		return MEChainMsg
+	case MTBlock:
+		return MEBlock
+	case MTDealProposal:
+		return MEDealProposal
+	case MTDeals:
+		return MEDeals
+	case MTDrawRandomParam:
+		return MEDrawRandomParam
+	case MTSignedVoucher:
+		return MESignedVoucher
+	case MTStorageAsk:
+		return MEStorageAsk
+	case MTAskResponse:
+		return MEAskResponse
+	case MTNetWorkResponse:
+		return MENetWorkResponse
+	case MTProviderDealState:
+		return MEProviderDealState
+	case MTClientDeal:
+		return MEClientDeal
+	default:
+		return MEUnknown
+	}
+}
---
+++ core/msg_enum_test.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestContainMsgType(t *testing.T) {
+	multiME := MEChainMsg + MEDeals + MEStorageAsk + MEProviderDealState
+	assert.Equal(t, ContainMsgType(multiME, MTChainMsg), true)
+	assert.Equal(t, ContainMsgType(multiME, MTDeals), true)
+	assert.Equal(t, ContainMsgType(multiME, MTStorageAsk), true)
+	assert.Equal(t, ContainMsgType(multiME, MTProviderDealState), true)
+
+	assert.Equal(t, ContainMsgType(multiME, MTUnknown), false)
+	assert.Equal(t, ContainMsgType(multiME, MTBlock), false)
+	assert.Equal(t, ContainMsgType(multiME, MTDealProposal), false)
+	assert.Equal(t, ContainMsgType(multiME, MTDrawRandomParam), false)
+	assert.Equal(t, ContainMsgType(multiME, MTSignedVoucher), false)
+	assert.Equal(t, ContainMsgType(multiME, MTAskResponse), false)
+	assert.Equal(t, ContainMsgType(multiME, MTNetWorkResponse), false)
+	assert.Equal(t, ContainMsgType(multiME, MTClientDeal), false)
+}
