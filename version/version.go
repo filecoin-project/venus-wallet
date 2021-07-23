ae9445fe34338878265adf36949459b3ad7d35cb
simlecode <hj1557986@gmail.com>
2021-07-23 17:03:17 +0800

    change version to 1.2.1

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.2.0"
+const BuildVersion = "1.2.1"
 
 var UserVersion = BuildVersion + CurrentCommit
 
