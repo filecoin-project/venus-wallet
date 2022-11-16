5ada4856e9068f9cbbe53961e238468e3b98b7f4
tanlang <lin.zexiao@qq.com>
2022-11-16 08:10:09 +0000

    chore: bump up version to 1.8.0

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.8.0-rc3"
+const BuildVersion = "1.8.0"
 
 var UserVersion = BuildVersion + CurrentCommit
 
